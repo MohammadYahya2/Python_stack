ad1f37b1cd3e2cf9dd9c6496dd9c36233958c3d9
Mohamamd Yahya <mohammad1.yahya2019@gmail.com>
2023-11-16 16:22:13 +0200

    Counter

--- _python/python_fundamentals/Functions Basic 2/main.py
+++ _python/python_fundamentals/Functions Basic 2/main.py
@@ -16,19 +16,22 @@ print(y)
 #3First Plus Length
 def firstPlusLength (list):
     return list[0]+len(list)
-y = firstPlusLength([5,6,7,8,9,10])
+y = firstPlusLength([8,6,7,8,9,10])
 print(y)
 
 #4Values Greater than Second
 def valuesGreaterthanSecond(list):
     list1 = []
     list_second_value = list[1]
+    
     for n in range(len(list)):
         if(n > list_second_value):
             list1.append(n)
+    print(len(list1))
     return list1
 y = valuesGreaterthanSecond([5,2,3,2,1,4])
 print(y)
+
 
 #5This Length, That Value
 def thisLengthThatValue(size, value):
